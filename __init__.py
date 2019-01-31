9dd509492e0542b5b87c10b86e7923748949fdb9
Mehrdad <mehrdadriyahi1378@gmail.com>
2019-01-31 15:07:14 +0330

    Project done!

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from k_formula import start_getting_graph
 
 
 def start():
-    print("1. K-formula to graph \n2. Graph to K-formula \n")
+    print("1. K-formula to graph \n2. Graph to K-formula")
     a = input()
     if a == '1':
         start_getting_formula()
